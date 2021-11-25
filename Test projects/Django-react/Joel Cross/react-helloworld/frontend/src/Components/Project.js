b01e5b9575b645f5b55fc158dc22f9e38f36745e
Joel Cross <joeldcross@gmail.com>
2021-11-24 20:59:46 -0500

    Styling changes

--- Test projects/Django-react/Joel Cross/react-helloworld/frontend/src/Components/Project.js
+++ Test projects/Django-react/Joel Cross/react-helloworld/frontend/src/Components/Project.js
@@ -19,7 +19,9 @@ const Project = (props) => {
                 <div><p className="project__category">{category}</p></div>
                 <div><p className="project__team">{team_members}</p></div>
             </div>
-            <span className="project__image-wrapper"><img className="project__image" src={placeholder} /></span>
+            <span className="project__image-wrapper">
+                <img className="project__image" src={placeholder} onClick={handleClick}/>
+            </span>
             <p className="project__description">{description}</p>
             <Outlet />
         </div>
