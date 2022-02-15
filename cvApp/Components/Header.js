68d28cbeeb56e66dd4b9c59320e43b9fe9be7740
TonyCansheep <tony@cansheep.ca>
2022-02-15 13:11:32 +0000

    upload files

--- cvApp/Components/Header.js
+++ cvApp/Components/Header.js
@@ -14,7 +14,7 @@ const styles = StyleSheet.create({
         width: "100%",
         height: 100,
         paddingTop: 40,
-        backgroundColor: "purple",
+        backgroundColor: "blue",
         alignItems: "center",
         justifyContent: "center"
     },
