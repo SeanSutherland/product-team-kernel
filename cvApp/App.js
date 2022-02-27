4dd9be53eb51e3bf1c4a8c9ab2ba608a05e6bed9
TonyCansheep <tony@cansheep.ca>
2022-02-27 16:43:53 +0000

    upload files

--- cvApp/App.js
+++ cvApp/App.js
@@ -1,14 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState, Component } from 'react';
 import { Button, StyleSheet, Text, View, Image, TouchableOpacity, Touchable } from 'react-native';
 import * as Permissions from 'expo-permissions';
 import Camera from "expo-camera";
 import * as ImagePicker from 'expo-image-picker';
-
 import Header from "./Components/Header";
 import Position from 'react-native/Libraries/Components/Touchable/Position';
-
 export default function App() {
+  
+  const[picture, setPicture] = useState("")  //picture url is in this state
+  const[imageOpen, setImageOpen] = useState(false) 
+  const[label, setLabel] = useState('Lamp')  //this gets updated when model changes it
   
   const pickFromGallery = async () =>{
     const {granted} = await Permissions.askAsync(Permissions.CAMERA_ROLL)
@@ -47,56 +49,88 @@ export default function App() {
           handleUpload(newfile)
         }
     }else{
-      Alert.alert("You need to give permissions")
+      Alert.alert("You need permissions")
     }
   }
-
   const handleUpload = (image)=>{
       const data = new FormData()
       data.append('file', image)
       data.append('upload_preset', 'lowerUpper1!')
       data.append("cloud_name", "lowerupper1")
-
+      setImageOpen(true)
+      console.log(imageOpen)
       fetch("https://api.cloudinary.com/v1_1/lowerupper1/image/upload", {
         method:"post",
         body:data
       }).then(res=>res.json()).
       then(data=>{
-        console.log(data)
         setPicture(data.url)
+        console.log(data)
       })
   } 
   
   return (
-    <View>
-      <Header title="CV App Template"></Header>
-      <View style={image.container}>
-        <Image 
-          style={image.logo}
-          source={require("./Components/qmindLogo.jpg")}/>
+    <>
+      <View style={imageOpen ? styles.invisible: styles.defaultScreen}>
+        <Header title="CV App Template"></Header> 
+        <View style={image.container}>
+          <Image 
+            style={image.logo}
+            source={require("./Components/qmindLogo.jpg")}/>
+        </View>
+        <View style={description.container}>
+          <Text style={description.title}>
+            Description
+          </Text>
+          <Text style={description.text}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eleifend purus nunc, non vulputate mauris mollis cursus. Etiam justo turpis, tincidunt in tellus non, tempus sagittis neque. Nullam non lacus at tellus eleifend dapibus. Curabitur finibus a erat sed commodo. Fusce sed pretium quam. Cras ac tempor enim. Aenean a egestas dui. Donec ullamcorper arcu sit amet nulla feugiat consectetur.
+          </Text>
+        </View>
+        <View style={buttons.screen}>
+          <TouchableOpacity style={buttons.button} onPress={() => pickFromCamera()}>
+            <Text style={buttons.text}>Take Picture</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={buttons.separate}>
+          <TouchableOpacity style={buttons.button} onPress={() => pickFromGallery()}>
+            <Text style={buttons.text}>Upload Picture</Text>
+          </TouchableOpacity>
+        </View>
       </View>
-      <View style={description.container}>
-        <Text style={description.title}>
-          Description
+      <View style={imageOpen ? styles.container: styles.invisible}>
+        <Button title = 'Back'  onPress={() => setImageOpen(false)} />
+        <Image
+          style={{width: '100%', height: '50%'}}                               //shitty old one fix dis
+          source={{uri : picture}}
+        /> 
+        <Text style = {styles.text}>
+          Label: {label}
         </Text>
-        <Text style={description.text}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eleifend purus nunc, non vulputate mauris mollis cursus. Etiam justo turpis, tincidunt in tellus non, tempus sagittis neque. Nullam non lacus at tellus eleifend dapibus. Curabitur finibus a erat sed commodo. Fusce sed pretium quam. Cras ac tempor enim. Aenean a egestas dui. Donec ullamcorper arcu sit amet nulla feugiat consectetur.
-        </Text>
       </View>
-      <View style={buttons.screen}>
-        <TouchableOpacity style={buttons.button} onPress={() => pickFromCamera()}>
-          <Text style={buttons.text}>Take Picture</Text>
-        </TouchableOpacity>
-      </View>
-      <View style={buttons.separate}>
-        <TouchableOpacity style={buttons.button} onPress={() => pickFromGallery()}>
-          <Text style={buttons.text}>Upload Picture</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
+      
+    </>
   );
 }
-
+const styles = StyleSheet.create({
+  defaultScreen: {
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  active: {
+    display: 'flex',
+  },
+  invisible: {
+    display: 'none',
+  },
+  text: {
+    marginTop: 10,
+    fontSize: 30,
+  },
+});
 const buttons = StyleSheet.create({
   screen: {
     paddingTop: 60,
@@ -119,7 +153,6 @@ const buttons = StyleSheet.create({
     color: 'black',
   },
 });
-
 const image = StyleSheet.create({
   container:{
     paddingTop: 20,
@@ -132,7 +165,6 @@ const image = StyleSheet.create({
     height: 180
   },
 });
-
 const description = StyleSheet.create({
   container:{
     paddingTop: 10,
@@ -149,4 +181,3 @@ const description = StyleSheet.create({
     fontSize: 16,
   }
 })
-
